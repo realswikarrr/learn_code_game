edf8b488fb4d7d91e7661f238dfed468cd8d0cd0
Swikar Adhikari <swikara082@gmail.com>
2023-05-02 20:49:22 +0930

    feat: 🛠️  added simple player function to the code

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+const player = new Player();
+
+// let bottom = y + height;
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.draw();
+  player.update();
+}
+
+animate();
