034fec0c634a1cfce9dd2b62a37648a839713a27
Swikar Adhikari <swikara082@gmail.com>
2023-05-04 12:25:40 +0930

    feat: 🛠️  added collision blocks to the map

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
+const parsedCollision = collisionLevel1.parse2D();
+
+const collisionBlocks = parsedCollision.createObjectsFrom2D();
+
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
@@ -30,6 +34,9 @@ function animate() {
   window.requestAnimationFrame(animate);
 
   backgroundLevel1.draw();
+  collisionBlocks.forEach((collisionBlocks) => {
+    collisionBlocks.draw();
+  });
 
   player.velocity.x = 0;
   if (keys.d.pressed) player.velocity.x = 5;
