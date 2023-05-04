53db339fdde62c8ae305ced9fba0460efc770b7e
Swikar Adhikari <swikara082@gmail.com>
2023-05-04 12:51:17 +0930

    feat: 🛠️  Added collision detection

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -1,8 +1,8 @@
 class Player {
-  constructor() {
+  constructor({ collisionBlocks = [] }) {
     this.position = {
-      x: 100,
-      y: 100,
+      x: 200,
+      y: 200,
     };
 
     this.velocity = {
@@ -10,13 +10,15 @@ class Player {
       y: 0,
     };
 
-    this.width = 100;
-    this.height = 100;
+    this.width = 25;
+    this.height = 25;
     this.sides = {
       bottom: this.position.y + this.height,
     };
 
     this.gravity = 1;
+
+    this.collisionBlocks = collisionBlocks;
   }
 
   draw() {
@@ -26,14 +28,68 @@ class Player {
 
   update() {
     this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-    this.sides.bottom = this.position.y + this.height;
 
-    // above bottom of canvas
-    if (this.sides.bottom + this.velocity.y < canvas.height) {
-      this.velocity.y += this.gravity;
-    } else {
-      this.velocity.y = 0;
+    this.checkForHorizontalCollision();
+
+    this.applyGravity();
+
+    this.checkForVerticalCollision();
+  }
+
+  checkForHorizontalCollision() {
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+      //   if a collision exists
+      if (
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
+        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      ) {
+        // collision on x axis going to lef
+        if (this.velocity.x < -0) {
+          this.position.x =
+            collisionBlock.position.x + collisionBlock.width + 0.01;
+          break;
+        }
+
+        if (this.velocity.x > 0) {
+          this.position.x = collisionBlock.position.x - this.width - 0.01;
+          break;
+        }
+      }
+    }
+  }
+
+  applyGravity() {
+    this.velocity.y += this.gravity;
+    this.position.y += this.velocity.y;
+  }
+
+  checkForVerticalCollision() {
+    // Check for vertical collision
+    for (let i = 0; i < this.collisionBlocks.length; i++) {
+      const collisionBlock = this.collisionBlocks[i];
+      //   if a collision exists
+      if (
+        this.position.x <= collisionBlock.position.x + collisionBlock.width &&
+        this.position.x + this.width >= collisionBlock.position.x &&
+        this.position.y + this.height >= collisionBlock.position.y &&
+        this.position.y <= collisionBlock.position.y + collisionBlock.height
+      ) {
+        if (this.velocity.y < 0) {
+          this.velocity.y = 0;
+          this.position.y =
+            collisionBlock.position.y + collisionBlock.height + 0.01;
+          break;
+        }
+
+        if (this.velocity.y > 0) {
+          this.velocity.y = 0;
+          this.position.y = collisionBlock.position.y - this.height - 0.01;
+          break;
+        }
+      }
     }
   }
 }
